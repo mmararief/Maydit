829cc378f5a6c3265e5d2d970567eabeb261c40d
ammar arief <ammararief321@gmail.com>
2023-07-04 16:10:38 +0700

    make it better

---
+++ src/components/Banner.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <h1>Selamat Datang! Maydit 1.0</h1>
+    </div>
+  );
+};
+
+export default Banner;
