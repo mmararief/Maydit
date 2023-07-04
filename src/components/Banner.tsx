bad61584fb16027b3acb109838bdc2863f9f4fd7
ammar arief <ammararief321@gmail.com>
2023-07-04 22:27:11 +0700

    fix banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,9 +1,33 @@
+import Link from "next/link";
 import React from "react";
+import { buttonVariants } from "./ui/Button";
 
 const Banner = () => {
+  const backgroundImage = {
+    backgroundImage: "url('http://webkadupa.my.id/kelas.jpg')",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  };
+
   return (
-    <div className="banner">
-      <h1>Selamat Datang Di Maydit 1.0</h1>
+    <div className="banner py-12" style={backgroundImage}>
+      <div className="container mx-auto text-center">
+        <h1 className="text-4xl sm:text-5xl text-white font-bold mb-4">
+          Selamat Datang Di Maydit 1.0
+        </h1>
+        <p className="text-lg sm:text-xl text-white">
+          Temukan komunitas berkualitas untuk pengembangan diri Anda
+        </p>
+        <Link
+          className={buttonVariants({
+            className:
+              "mt-6 px-6 py-3 rounded-md hover:bg-blue-600 transition-colors duration-300 ease-in-out",
+          })}
+          href={`/r/create`}
+        >
+          Create Community
+        </Link>
+      </div>
     </div>
   );
 };
